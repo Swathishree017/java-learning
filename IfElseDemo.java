a82b275b6c17948130213dcbe1e752e20881cd3b
Swathishree017 <swathishreeram02@gmail.com>
2026-01-21 12:04:16 +0530

    Create IfElseDemo.java

---
+++ IfElseDemo.java
@@ -0,0 +1,13 @@
+// Demonstrates if-else condition
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is not positive");
+        }
+    }
+}
