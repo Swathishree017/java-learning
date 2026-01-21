0cf3ca42d76bbd6e2f176a0f382f47066fe53533
Swathishree017 <swathishreeram02@gmail.com>
2026-01-21 12:03:51 +0530

    Create VariablesDemo.java

---
+++ VariablesDemo.java
@@ -0,0 +1,15 @@
+// Demonstrates basic variables and data types
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 30000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
