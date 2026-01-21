f9ea37532e2012993720fea4d94c5f367f728a40
Swathishree017 <swathishreeram02@gmail.com>
2026-01-21 12:05:27 +0530

    Create MethodDemo.java

---
+++ MethodDemo.java
@@ -0,0 +1,12 @@
+// Demonstrates a simple method
+
+public class MethodDemo {
+
+    static void greet() {
+        System.out.println("Hello, welcome to Java learning");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
