44f174ae8402f981dbed782bbc8ecd964355f23c
Swathishree017 <swathishreeram02@gmail.com>
2026-01-21 12:02:43 +0530

    Create BasicCalculator.java

---
+++ BasicCalculator.java
@@ -0,0 +1,12 @@
+// Simple calculator using Java basics
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+    }
+}
